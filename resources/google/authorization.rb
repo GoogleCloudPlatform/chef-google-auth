88c7ee3a53e4b9554cc235e89d08a89bcb113c9e
Nelson Araujo <nelsona@google.com>
2017-12-06 16:45:19 -0800

    New authorization w/ machineaccount & applicationdefault suppot
    Change-Id: I6a4a748e12bc1a2ae966f1bd63a1b3f514e7f288

--- resources/google/authorization.rb
+++ resources/google/authorization.rb
@@ -1,5 +1,4 @@
-# Copyright 2016 Google Inc.
-#
+# Copyright 2017 Google Inc.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -30,6 +29,8 @@
 #         .from_service_account_json!(
 #             File.join(File.expand_path('~'), "my_account.json"))
 #         .authorize Google::Apis::ComputeV1::ComputeService.new
+#
+# TODO(nelsona): Add support gcloud's beta "app default credential"
 
 require 'googleauth'
 require 'json'
@@ -43,18 +44,50 @@ module Google
       Gem::Version.new(RUBY_VERSION.dup) >= Gem::Version.new('2.0.0')
     end
 
-    # rubocop:disable Performance/Caller
     def self.ensure_two!
-      callee = caller[0][/`([^']*)'/, 1]
+      callee = caller(1..1).first[/`([^']*)'/, 1]
       raise "Ruby ~> 2.0.0 required for '#{callee}'" unless Ruby.two?
     end
-    # rubocop:enable Performance/Caller
   end
 
   require 'google/api_client/client_secrets' if Google::Ruby.two?
 
   # A class to aquire credentials and authorize Google API calls.
   class Authorization
+    LEGACY_CRED_FILE =
+      File.join(ENV['HOME'], '.config', 'gcloud', 'credentials').freeze
+    CRED_DB_FILE =
+      File.join(ENV['HOME'], '.config', 'gcloud', 'credentials.db').freeze
+    DEFAULT_APPLICATION_FILE =
+      File.join(ENV['HOME'], '.config', 'gcloud',
+                'application_default_credentials.json').freeze
+
+    # A helper class to retrieve the machine account token
+    class MachineAccount
+      METADATA_MASK = ['http://metadata/computeMetadata/v1/instance/',
+                       'service-accounts/{{id}}/token'].freeze
+      METADATA_HEADER = { key: 'Metadata-Flavor', value: 'Google' }.freeze
+
+      def initialize(account_id)
+        raise 'Missing argument for account_id' if account_id.empty?
+        @account_id = account_id
+        @uri = URI.join(*METADATA_MASK.map do |p|
+                          p.gsub('{{id}}', @account_id)
+                        end)
+        @request = Net::HTTP::Get.new(@uri.request_uri)
+        @request[METADATA_HEADER[:key]] = METADATA_HEADER[:value]
+      end
+
+      def apply!(result)
+        response = Net::HTTP.new(@uri.host, @uri.port).request(@request)
+        raise 'Cannot retrieve authentication token from metadata server' \
+          unless response.is_a?(Net::HTTPOK)
+        result[:authorization] =
+          ['Bearer', JSON.parse(response.body)['access_token']].join(' ')
+        self
+      end
+    end
+
     def initialize
       @authorization = nil
       @scopes = []
@@ -79,8 +112,17 @@ module Google
       self
     end
 
+    def account_id!(account_id)
+      @account_id = account_id
+      self
+    end
+
     def from_user_credential!
-      Google::Ruby.ensure_two!
+      Google::Ruby.ensure_two! # TODO(nelsona): Ensure we can run with Ruby 1.9
+      unless File.exist?(LEGACY_CRED_FILE)
+        raise ['Legacy credentials not found and new credentials.db not yet',
+               'implemented. For the time being use another provider'].join(' ')
+      end
       hash = make_secrets_hash(find_credential)
       @authorization = Google::APIClient::ClientSecrets.new(hash)
                                                        .to_authorization
@@ -97,7 +139,19 @@ module Google
     end
 
     def from_application_default_credentials!
-      raise NotImplementedError, ':application_default_credentials'
+      unless File.exist?(DEFAULT_APPLICATION_FILE)
+        raise ['Application credentials not present.',
+               "Run 'gcloud auth application-default login'"].join(' ')
+      end
+      hash = make_secrets_hash(JSON.parse(File.read(DEFAULT_APPLICATION_FILE)))
+      @authorization = Google::APIClient::ClientSecrets.new(hash)
+                                                       .to_authorization
+      self
+    end
+
+    def from_machine_account!
+      @authorization = MachineAccount.new(@account_id)
+      self
     end
 
     private
@@ -118,14 +172,8 @@ module Google
       req
     end
 
-    def credentials
-      JSON.parse(
-        File.read(File.join(ENV['HOME'], '.config', 'gcloud', 'credentials'))
-      )
-    end
-
     def find_credential
-      credentials['data'].each do |entry|
+      JSON.parse(FILE.read(CREDENTIAL_FILE))['data'].each do |entry|
         if entry['credential']['_class'] == 'OAuth2Credentials'
           return entry['credential']
         end
